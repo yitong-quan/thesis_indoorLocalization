054321106a3ca9e9ed2551aed34f971f834fc5c2
yitong-quan <xmlysxr@gmail.com>
2017-06-13 18:41:33 +0200

    dataFromBasisStationCanBeUsed; addDataToBeProcessed

--- communicate_with_basis_station_in_matlab/try_to_communicate_with_basis_station_in_matlab.m
+++ communicate_with_basis_station_in_matlab/try_to_communicate_with_basis_station_in_matlab.m
@@ -2,40 +2,62 @@
 % instrfind
 close all; clear all;
 delete(instrfindall)
-% s = serial('COM10'); set(s,'BaudRate',115200);
+
+% set serial port properties
+%s = serial('COM10'); set(s,'BaudRate',115200);
 s=serial('COM24', 'BaudRate', 115200);
 set(s,'Parity', 'even');
-set(s,'Timeout', 1);
+set(s,'Timeout', 0.7); % TODO: adjust. sampling rate is 1.5, 0.66s <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 s.Terminator = 'CR/LF';
 fopen(s);
-% fprintf(s, '%s', 'setNUMBER: 1\n\r');
-%out = fgetl(s);
-%out = fread(s);
-[out, count] = fscanf(s, '%s', 300);
-[out1, count1] = fscanf(s);
-[out2, count2] = fscanf(s);
-fprintf(s, 'setNUMBER: 1');
+%[out, count] = fscanf(s, '%s', 300); [out1, count1] = fscanf(s); [out2, count2] = fscanf(s);
+
+% set nodeNumber & nodeID
+nodeNumber = 3; %<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+fprintf(s, 'setNUMBER: %d\n', nodeNumber); % '\n' is important here, since... 
+% ...'%s'Specify a format for the data that does not include the terminator, or configure the terminator to empty.  
 [out3, count3, msg3] = fscanf(s); % out3 should be 'Write down the 16 bit IDs of 1 Nodes in HEX'
-fprintf(s, 'setNODES: [0x6E6E]');
+fprintf(s, 'setNODES: [0x2020, 0x4D4D, 0x1C1C]'); %<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 fprintf(s, 'find 0x310D, 1');
 pause(1); % TODO: for the first time wake up, will need more time>> non fixed time needed
 
-numberSend = 50;
-NodeID = strings([numberSend,1]);
-dist = strings([numberSend,1]);
-dt = strings([numberSend,1]);
-count_data = strings([2,numberSend]);
-msg_data = strings([2,numberSend]);
+numberSend = 28;
+
+% construct the data
+id_dist = strings([numberSend,nodeNumber]); % returns an m-by-n array of strings with no characters
+%dist = strings([numberSend,nodeNumber]);
+timeStamp = strings([numberSend,1]);
+count_data = strings([numberSend,nodeNumber]); % count_data = strings([2,numberSend,nodeNumber]);
+msg_data = strings([numberSend,nodeNumber]); % msg_data = strings([2,numberSend,nodeNumber]);
+
+% send out 'find' command, record and process data from the basis station
 for i = 1:numberSend
-    pause(0.1); %% each loop cost time +0.6
+    %pause(0.01); %% each loop cost time +0.6
     fprintf(s, 'find 0x310D, 1');
-    [NodeID(i), count_data(1,i), msg_data(1,i)] = fscanf(s);
-    [dist(i), count_data(2,i), msg_data(2,i)] = fscanf(s);
-    dt(i) = datestr(now,'mmmm dd, yyyy HH:MM:SS.FFF');
+    timeStamp(i) = datestr(now,'SS.FFF');
+    %timeStamp(i) = datestr(now,'HH:MM:SS.FFF mmm dd yyyy');
+    for j = 1:nodeNumber
+        %pause(0.02);
+        [id_dist(i,j), count_data(i,j), msg_data(i,j)] = fscanf(s);
+        % add this if() to keep the data structure in right order when the
+        % communication with nodes are not achieved
+        if (msg_data(i,j) == 'A timeout occurred before the Terminator was reached.')
+            %pause(0.1);
+            id_dist(i,j) = fscanf(s);
+        end
+        %[dist(i,j), count_data(2,i,j), msg_data(2,i,j)] = fscanf(s);
+    end
 end    
-data = [NodeID, dist, dt];
+data = [id_dist, timeStamp];
 data = regexprep(data,'\r\n|\n|\r',''); % remone '\r\n' from all the strings
+% distance with double format
+%dist = str2double(data(:,3:4)); 
 fclose(s)
 delete(s)
 clear s
 delete(instrfindall)
+
+%% for testing
+time = str2double(data(:,4));
+timeInterval = diff(time);
+timeInterval_mean = (sum(timeInterval)- min(timeInterval)-max(timeInterval))/(length(timeInterval)-2);
