c15e29082b9392f7d486e2a08c6b019adada823e
yitong-quan <xmlysxr@gmail.com>
2017-06-19 10:33:25 +0200

    Q matrix, get rid of randn(2.1), which should not be in Q

--- KalmanFilter/KF_traj/KF_traj_noisy_meas_3nodes.m
+++ KalmanFilter/KF_traj/KF_traj_noisy_meas_3nodes.m
@@ -35,7 +35,8 @@ H_symbolic = jacobian(Z_e, x_m);
 G = [dt^2/2*eye(2);dt*eye(2)];
 sigma = 0.1;
 RandnForGenTracj = randn(2, 1); % TODO: an besten, the randn(2, 1) should be the same as the one generated the traj
-Q = G * (sigma * diag(RandnForGenTracj))^2 * G'; %since displacement cause by acceleration is G * sigma * randn(2,1) {acceleration is  sigma * randn(2,1)}
+%Q = G * (sigma * diag(RandnForGenTracj))^2 * G'; %since displacement cause by acceleration is G * sigma * randn(2,1) {acceleration is  sigma * randn(2,1)}
+Q = G * sigma^2 * G';
 % measurement noise covariance R
 R = 1.0 * eye(3); % shoule be relative to the value of 'noiseLevelForMeasurements.mat' in folder 'goodTraj01'
 % TODO, correct Q & R, they are square matrices
@@ -53,20 +54,20 @@ for i = 2:size(measurements_data_noisy,2)
     % time update
     x_minus= A * X(:, i - 1) + B * u; % TODO, think about if the accelerations should be included into state vectors
     P_minus = A * P(:, :, i-1) * A' + Q; 
-%     % measurement update
-%         % Never use the inverse of a matrix to solve a linear system Ax=b with 
-%         % x=inv(A)*b, because it is slow and inaccurate.
-%         % Replace inv(A)*b with A\b, Replace b*inv(A) with b/A, replace A*inv(B)*C with A*(B\C).
-%         % HERE replace K = P_minus * H' * invs(H * P_minus * H' +R) with P_minus * H' / (H * P_minus * H' +R)    
-%     %try a very naiiv way to calculate the H, should be improved
-%     %definitely. TODO
-%     z_expected = [
-%                     sqrt( (positionOfNodes(1,1) - x_minus(1))^2 + (positionOfNodes(2,1) - x_minus(2))^2 );
-%                     sqrt( (positionOfNodes(1,2) - x_minus(1))^2 + (positionOfNodes(2,2) - x_minus(2))^2 );
-%                     sqrt( (positionOfNodes(1,3) - x_minus(1))^2 + (positionOfNodes(2,3) - x_minus(2))^2 )
-%                  ];
-%     H = z_expected/x_minus;
-    H = eval(subs( subs(H_symbolic, x_m, x_minus), N_x_n, positionOfNodes(:,1:3)));
+    % measurement update
+        % Never use the inverse of a matrix to solve a linear system Ax=b with 
+        % x=inv(A)*b, because it is slow and inaccurate.
+        % Replace inv(A)*b with A\b, Replace b*inv(A) with b/A, replace A*inv(B)*C with A*(B\C).
+        % HERE replace K = P_minus * H' * invs(H * P_minus * H' +R) with P_minus * H' / (H * P_minus * H' +R)    
+    %try a very naiiv way to calculate the H, should be improved
+    %definitely. TODO
+    z_expected = [
+                    sqrt( (positionOfNodes(1,1) - x_minus(1))^2 + (positionOfNodes(2,1) - x_minus(2))^2 );
+                    sqrt( (positionOfNodes(1,2) - x_minus(1))^2 + (positionOfNodes(2,2) - x_minus(2))^2 );
+                    sqrt( (positionOfNodes(1,3) - x_minus(1))^2 + (positionOfNodes(2,3) - x_minus(2))^2 )
+                 ];
+    H = z_expected/x_minus;
+%    H = eval(subs( subs(H_symbolic, x_m, x_minus), N_x_n, positionOfNodes(:,1:3)));
     K_k = P_minus * H' / (H * P_minus * H' +R);
     X(:, i) = x_minus + K_k * (z(:, i) - H * x_minus);
     P(:, :, i) = (eye(length(x_0)) - K_k * H) * P_minus;
