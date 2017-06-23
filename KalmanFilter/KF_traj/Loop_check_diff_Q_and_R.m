56343e71e2e13a0797769e2171261cb984eaca1a
yitong-quan <xmlysxr@gmail.com>
2017-06-23 14:11:45 +0200

    added fig from same R and noise to generate traj

--- KalmanFilter/KF_traj/Loop_check_diff_Q_and_R.m
+++ KalmanFilter/KF_traj/Loop_check_diff_Q_and_R.m
@@ -1,14 +1,16 @@
 
 
-nodesNumbers = 3; % 3 or 4
+nodesNumbers = 4; % 3 or 4
 i = 0.1/3; n= 0;
-i_max = 10; j_max = 10;
-while i <= i_max % factor_Q
+i_max = 100; j_max = 100;
+%while i <= i_max % factor_Q
+while i <=0.1/3 % factor_Q    
     j = 0.1/3;
-    while j <= j_max % factor_R
+    %while j <= j_max % factor_R
+    while j <= 0.1/3 % factor_R
 %         KF_traj_noisy_meas_3nodes(i,j);
 %         KF_traj_noisy_meas_4nodes(i,j);
-        KF_traj_noisy_meas_3or4nodes(i,j,nodesNumbers,true, 1); %  measurements_missing, meas_missing_rate~
+        KF_traj_noisy_meas_3or4nodes(i,j,nodesNumbers,false, 0); %  measurements_missing, meas_missing_rate~
         fprintf('i:%f, max:%d;      j:%f, max:%d;\n', i, i_max, j, j_max);
         j = j * 3;
         n=n+1;
