2b88628930c8169aef7ed35ccee0aac3c679bc2d
yitong-quan <xmlysxr@gmail.com>
2017-06-25 20:29:56 +0200

    added calibreation meas

--- communicate_with_basis_station_in_matlab/try_to_communicate_with_basis_station_in_matlab.m
+++ communicate_with_basis_station_in_matlab/try_to_communicate_with_basis_station_in_matlab.m
@@ -5,7 +5,8 @@ delete(instrfindall)
 
 % set serial port properties
 %s = serial('COM10'); set(s,'BaudRate',115200);
-s=serial('COM24', 'BaudRate', 115200);
+%s=serial('COM24', 'BaudRate', 115200);
+s=serial('COM4', 'BaudRate', 115200); % small PC
 set(s,'Parity', 'even');
 set(s,'Timeout', 0.7); % TODO: adjust. sampling rate is 1.5, 0.66s <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 s.Terminator = 'CR/LF';
@@ -18,14 +19,14 @@ fprintf(s, 'setNUMBER: %d\n', nodeNumber); % '\n' is important here, since...
 % ...'%s'Specify a format for the data that does not include the terminator, or configure the terminator to empty.  
 [out3, count3, msg3] = fscanf(s); % out3 should be 'Write down the 16 bit IDs of 1 Nodes in HEX'
 %fprintf(s, 'setNODES: [0x1C1C, 0x2020, 0x3E3E, 0x4D4D, 0x6E6E]'); %<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-fprintf(s, 'setNODES: [0x6E6E]');
+fprintf(s, 'setNODES: [0x1C1C]');
 %fprintf(s, 'setNODES: [0x1C1C, 0x2020, 0x3E3E]');
 FindCommand = 'find 0x310D, 1'; %<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 fprintf(s, FindCommand);
 [out0, count0, msg0] = fscanf(s);
 pause(2); % TODO: for the first time wake up, will need more time>> non fixed time needed
 
-numberSend = 10;
+numberSend = 100;
 
 % construct the data
 id_dist = strings([numberSend,nodeNumber]); % returns an m-by-n array of strings with no characters
