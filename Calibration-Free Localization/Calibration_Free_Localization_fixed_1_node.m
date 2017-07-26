8ced2b969b41aac79517a3b40f0e9b25cf7e8b0a
yitong-quan <xmlysxr@gmail.com>
2017-07-26 17:59:11 +0200

    calibration_free_lo.m with and ohne fixed nodes

--- Calibration-Free Localization/Calibration_Free_Localization_fixed_1_node.m
+++ Calibration-Free Localization/Calibration_Free_Localization_fixed_1_node.m
@@ -1,8 +1,11 @@
 clear 
 %% 
 % one D
-node_posi_sym = sym('n_%d', [1 2]);
+%node_posi_sym = sym('n_%d', [1 2]);
+node_posi_sym = sym('n_%d', [1 2]); % fix 1 out of the 2 ndoes
+node_posi_sym(2) = 3;
 tag_p_sym = sym('tag_p_%d', [1 7]);
+
 f_sym = sym('f_%d', [length(node_posi_sym), length(tag_p_sym)]);
 nodes_p = [-7, 3];
 tag_p = linspace(-6,0,7);
@@ -17,17 +20,19 @@ for l = 1:size(f_sym)
     b = [b f_sym(l,:)];
 end    
 b = b.';
-u = [tag_p_sym, node_posi_sym].';
+u = [tag_p_sym, node_posi_sym(1:end-1)].';
 Q = jacobian(b, u);
 d_w_sym = Q.' * b;
 % cost function
 F_sym = sum(sum((f_sym).^2));
 
-u_0 = [-6.3 -5.9 -4 -3 -2.0 -1 0, -7 3]'; %[ tag node]
+%u_0 = [-6.3 -5.9 -4 -3 -2.0 -1 0, -7 3]'; %[ tag node]
+%u_0 = [-7.3 -1.9 -9.9 -2.5 -2.9 -10.9 0.9, -7.9]'; %[ tag node] fix the last node
+u_0 = rand(8,1); %[ tag node] fix the last node
 u_tilde = inf;
-gama = 0.05; %<<<<<
+gama_init = 0.5; %<<<<<
 epslon_taget = 0.00001;
-iter_max = 200;
+iter_max = 900;
 iter = 1;
 
 u = u_0;
@@ -35,21 +40,22 @@ U = u_0;
 F = inf;
 cost = inf;
 
-while iter < iter_max && cost > 1e-10 %norm(u_tilde) >= epslon_taget
-    d_w =  eval(subs(d_w_sym, [tag_p_sym, node_posi_sym].', u));
+while iter < iter_max && cost > 0.0000001 %norm(u_tilde) >= epslon_taget
+    gama = rand*gama_init;  %<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< set rand factor for gamma
+    d_w =  eval(subs(d_w_sym, [tag_p_sym, node_posi_sym(1:end-1)].', u));
         % todo plot the cost function : sum((f_ij)^2)
-    cost = eval(subs(F_sym, [tag_p_sym, node_posi_sym ].', u));
+    cost = eval(subs(F_sym, [tag_p_sym, node_posi_sym(1:end-1) ].', u));
     F = [F,  cost];
     u_tilde = gama * d_w;
     u = u - u_tilde;
     U = [U, u];
     iter = iter + 1;
-    %gama = 0.8*gama;
+    
 end
 
 plot(F, '-*r');
-disp(u');
-disp(cost);
+u'
+cost
 %true posi
 % plot(nodes_p, zeros(size(nodes_p)), '*b');
 % hold on; plot(tag_p, zeros(size(tag_p)), '*r')
--- communicate_with_basis_station_in_matlab/calibrations/outdoor/calib_read_file_generate_matrix__no_timeStamp.m
+++ communicate_with_basis_station_in_matlab/calibrations/outdoor/calib_read_file_generate_matrix__no_timeStamp.m
@@ -2,7 +2,7 @@
 %% 
 clear;
 %fileID = fopen('0x1C1C-10m.log'); %'0x2020, 0x3E3E, 0x4D4D, 0x6E6E-2m.log'
-fileID = fopen('599cm_all_.log'); 
+fileID = fopen('809cm_all_.log'); 
 %{
 % tline = fgetl(fid);
 % while ischar(tline)
