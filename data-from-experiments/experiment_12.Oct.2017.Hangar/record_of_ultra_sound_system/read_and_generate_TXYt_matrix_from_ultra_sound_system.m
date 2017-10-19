e41bbadd327c531901a09c6a195d438e33dff87c
yitong-quan <xmlysxr@gmail.com>
2017-10-19 23:29:21 +0200

    change structure; finisich reading and plotting better data from Joan 'positionsBetterFromJoan'

--- data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_ultra_sound_system/read_and_generate_TXYt_matrix_from_ultra_sound_system.m
+++ data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_ultra_sound_system/read_and_generate_TXYt_matrix_from_ultra_sound_system.m
@@ -1,8 +1,11 @@
 %data contain: [timeref	est_sendtimepos_x	pos_y	vel_x	vel_y sigma	valid	senderGuid]
 function read_and_generate_TXYt_matrix_from_ultra_sound_system(i)
 %M = dlmread(filename,delimiter,R1,C1) starts reading at row offset R1 and column offset C1. For example, the offsets R1=0, C1=0 specify the first value in the file.
+
+%folder_name = 'positionBeforeMakedBetterFromJoan';
+folder_name = 'positionsBetterFromJoan';
 file_str_pre = 'positions';
-file_name = [file_str_pre, num2str(i), '.log'];
+file_name = [folder_name, '/', file_str_pre, num2str(i), '.log'];
 
 data  = dlmread(file_name,'	',1 ,0);
 data_TXY = [data(:,1), data(:,3), data(:,4)];
@@ -15,7 +18,7 @@ for j = 1:size(data_TXY,1)
         break
     end
 end    
-mat_str = ['mat_', file_str_pre, num2str(i), '.mat'];
+mat_str = [folder_name, '/mat_', file_str_pre, num2str(i), '.mat'];
 save(mat_str,'data_TXYt');
 %save mat_str data;
 end
