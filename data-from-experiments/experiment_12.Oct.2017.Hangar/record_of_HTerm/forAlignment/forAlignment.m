0c0a39b665bc035845eaea78de58296e5481a084
yitong-quan <xmlysxr@gmail.com>
2017-10-20 18:45:39 +0200

    + .avi and .fig for comparision after RT UWB&USS(betterDataAfterJoan)

--- data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_HTerm/forAlignment/forAlignment.m
+++ data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_HTerm/forAlignment/forAlignment.m
@@ -1,9 +1,15 @@
+clear
+%% calculate the translateion matrix R and T
 expNum = 1;
+
 str_ekf = ['../estimated_posi_with_timeSt_EKF_experi', num2str(expNum), '.mat'];
-str_US = ['../../record_of_ultra_sound_system/mat_positions', num2str(expNum), '.mat'];
 data_ekf = importdata(str_ekf);
+% str_US = ['../../record_of_ultra_sound_system/positionBeforeMakedBetterFromJoan/mat_positions', num2str(expNum), '.mat'];
+str_US = ['../../record_of_ultra_sound_system/positionsBetterFromJoan/mat_positions', num2str(expNum), '.mat'];
 data_US = importdata(str_US);
 data_US = data_US';
+% replace outliar with the closest 'correct' position
+data_US(2:3,1:3) = repmat(data_US(2:3,4),1,3);
 % nearestpoint(shortArray,longArray)
 timeStampNeed_data_US = nearestpoint(data_ekf(5,:), data_US(4,:));
 %{
@@ -20,24 +26,56 @@ data_US_trimed = data_US(:,timeStampNeed_data_US);
 % theta = 0;
 % t0 = zeros(2,1);
 % M0 = [ [cos(theta), -sin(theta); sin(theta), cos(theta)], t0];
-x0 = zeros(1,3); % M = [theta, t1, t2]
+x0 = 100*rand(1,3); % M = [theta, t1, t2]
 options = optimoptions(@lsqnonlin,'Algorithm','levenberg-marquardt','Display','iter','MaxIterations',2000);
 [x,resnorm] = lsqnonlin(@myfun,x0,[],[],options);
 M = [ [cos(x(1)), -sin(x(1)); sin(x(1)), cos(x(1))], x(2:3)'];
+%% plot the location of EKF and the location of the USS
 afterRT_data_US_trimed = M(:,[1,2]) * data_US_trimed(2:3,:) + M(:,end);
 figure; plot(data_ekf(1,:), data_ekf(2,:),'r-o'); hold on;
 plot(afterRT_data_US_trimed(1,:), afterRT_data_US_trimed(2,:), 'b-+');
 plot(data_US_trimed(2,:), data_US_trimed(3,:), 'g-+');
+ legend('EKF', 'USS trimed', 'USS trimed RT');
 title_stri = ['exp', num2str(expNum), '  theta=', num2str(x(1)), '  t1=', num2str(x(2)), '  t2=', num2str(x(3))];
 title(title_stri);
-   daspect([10,10,10]);
+daspect([10,10,10]);
 
+%% plot the traj of EKF(moving) and the location of the USS(after RT)
+figure; hold on;
+title_stri = ['exp', num2str(expNum), ' comparision of EKF and USS'];
+title(title_stri);
+plot(afterRT_data_US_trimed(1,:), afterRT_data_US_trimed(2,:), 'b+');
+time_diff = diff(data_ekf(5,:))'; % unit second
+pause(3);
+	pause_time = 0.002*[time_diff; 2];
+    for j = 5:size(data_ekf,2) %1:size(X,2)-9 
+        
+        h3 = plot(afterRT_data_US_trimed(1,j-4:j), afterRT_data_US_trimed(2,j-4:j), '-ob');
+        hold on;
+        h2 = plot(data_ekf(1,j-4:j), data_ekf(2,j-4:j), '-or'); %h2 = plot(X(1,j:j+9), X(2,j:j+9), '-+r');
+        legend('USS loc', 'USS', 'EKF');
+        Fram(j-4) = getframe(gcf);
+        
+        % pause(pause_time(j));
+        delete(h2);
+        delete(h3);
+    end
+    video_str = ['video_comparision', num2str(expNum), '.avi'];
+    video = VideoWriter(video_str);
+    open(video)
+    writeVideo(video, Fram)
+    close(video)
+
+
+%%
 % afterRT = R0 * nodes_optimal + t0;
 function F = myfun(x)
 
-expNum = 1;
+expNum = 3;
 str_ekf = ['../estimated_posi_with_timeSt_EKF_experi', num2str(expNum), '.mat'];
-str_US = ['../../record_of_ultra_sound_system/mat_positions', num2str(expNum), '.mat'];
+% str_US = ['../../record_of_ultra_sound_system/positionBeforeMakedBetterFromJoan/mat_positions', num2str(expNum), '.mat'];
+str_US = ['../../record_of_ultra_sound_system/positionsBetterFromJoan/mat_positions', num2str(expNum), '.mat'];
+
 data_ekf = importdata(str_ekf);
 data_US = importdata(str_US);
 data_US = data_US';
--- data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_ultra_sound_system/plot_positions_without_timing.m
+++ data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_ultra_sound_system/plot_positions_without_timing.m
@@ -1,12 +1,17 @@
+clear;
+%%
 %folder_name = 'positionBeforeMakedBetterFromJoan';
 folder_name = 'positionsBetterFromJoan';
 file_name = 'mat_positions';
-experinumber = 1;
+experinumber = 6;
 data_TXYt = importdata([folder_name, '/', file_name, num2str(experinumber), '.mat']);
+time_diff = diff(data_TXYt(:,4));
 
 h=figure; hold on;
 title(['experiment', num2str(experinumber)]);
 plot(data_TXYt(:,2), data_TXYt(:,3), '+');
+    daspect([10,10,10]);
+    
 switch folder_name
     case 'positionsBetterFromJoan'
         savefig(h, [folder_name, '/USS experiment After', num2str(experinumber), '.fig']);
@@ -15,10 +20,17 @@ switch folder_name
     otherwise
         warning('please specify folder name: before or after making better')
 end
-        pause(1.5);
+       % pause(1.5);
+pause_time = 0.00002*[time_diff; 2];        
 for j = 10:size(data_TXYt,1)
     h2 = plot(data_TXYt(j-9:j,2), data_TXYt(j-9:j,3), '-or');
-    daspect([10,10,10]);
-    pause(0.0003);
+    Fram(j-9) = getframe(gcf);
+    % pause(pause_time(j));
     delete(h2);
 end
+
+    video_str = ['videoUUS/video_USS_experiment', num2str(experinumber), '.avi'];
+    video = VideoWriter(video_str);
+    open(video)
+    writeVideo(video, Fram)
+    close(video)
