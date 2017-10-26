3afa627786d750555ce106a60bd9ca4114101301
yitong-quan <xmlysxr@gmail.com>
2017-10-26 19:05:35 +0200

    remove the outlier and replace with 0 in the estimation position on 'KF_using_HTerm_data.m'

--- data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_HTerm/test.m
+++ data-from-experiments/experiment_12.Oct.2017.Hangar/record_of_HTerm/test.m
@@ -1,13 +1,23 @@
-%% plot traj moving
-     h = plot(X(1,:), X(2,:), 'ob');
-     hold on;
-    X= estimated_posi_with_timeSt;
-	pause_time = 0.2*[time_diff'; 2];
-    for j = 51:size(X,2) %1:size(X,2)-9 
-        h2 = plot(X(1,j-4:j), X(2,j-4:j), '-+r'); %h2 = plot(X(1,j:j+9), X(2,j:j+9), '-+r'); 
-            daspect([10,10,10]);
-            title(num2str(j));
-        % pause(pause_time(j));
-        delete(h2);
-        
-    end
+figure; [y,i,xmedian,xsigma] = hampel(measurements_data_noisy(5,:));
+n = 1:length(measurements_data_noisy);
+plot(n,measurements_data_noisy(5,:))
+hold on
+plot(n,xmedian-3*xsigma,n,xmedian+3*xsigma)
+plot(find(i),measurements_data_noisy(5,i),'sk')
+hold off
+legend('Original signal','Lower limit','Upper limit','Outliers')
+
+
+% %% plot traj moving
+%      h = plot(X(1,:), X(2,:), 'ob');
+%      hold on;
+%     X= estimated_posi_with_timeSt;
+% 	pause_time = 0.2*[time_diff'; 2];
+%     for j = 51:size(X,2) %1:size(X,2)-9 
+%         h2 = plot(X(1,j-4:j), X(2,j-4:j), '-+r'); %h2 = plot(X(1,j:j+9), X(2,j:j+9), '-+r'); 
+%             daspect([10,10,10]);
+%             title(num2str(j));
+%         % pause(pause_time(j));
+%         delete(h2);
+%         
+%     end
