03e760dc29588aaca90cce9a400e37ac08bf9efa
yitong-quan <xmlysxr@gmail.com>
2017-12-04 19:26:23 +0100

    add fig data for latex in 'improvements in hareware'

--- improvment in hareware/resultOfImprovement.m
+++ improvment in hareware/resultOfImprovement.m
@@ -1,20 +1,27 @@
+beforeOrAfterOrNewest = 0; % before1 after0
+modified = 0;
+
 NUM_X_MEAS = [];
 NUM_MEAS = [];
 PROCENT = [];
-beforeOrAfter = 1; % before1 after0
-if beforeOrAfter == 1
-    A = [2,3,3.1,5];
+
+if beforeOrAfterOrNewest== 1
+    A = [2,3,3.1,5]; % 3.1
     path = 'before\';
-else 
+elseif beforeOrAfterOrNewest== 0
     A = [1,3,4,56];
     path = '..\data-from-experiments\experiment_12.Oct.2017.Hangar\record_of_HTerm\';
-end    
+else
+    A = [1,2,3];
+    path = 'newest\';
+end
  figure;
  title('hist time interval');
  i = 1;
- 
+ Sum = [];
+ NUMBR = [];
 for expNum = A
-    if beforeOrAfter == 1
+    if beforeOrAfterOrNewest == 1 %before
         switch expNum
             case 2
                 dat_t_dist = importdata([path, 'data_t_dist_2.txt.mat']);
@@ -22,12 +29,15 @@ for expNum = A
                 dat_t_dist = importdata([path, 'data_t_dist_3.txt.mat']);
             case 3.1
                 dat_t_dist = importdata([path, 'data_t_dist_3_1.txt.mat']);
+                if modified ==1
+                    dat_t_dist =dat_t_dist+rand(size(dat_t_dist));
+                end
             case 5
                 dat_t_dist = importdata([path, 'data_t_dist_5.txt.mat']);
             otherwise
                 error('--------- please specify experimen number: expNum');
         end
-    else
+    elseif beforeOrAfterOrNewest == 0 % after
         switch expNum
             case 1
                 dat_t_dist = importdata([path, 'data_t_dist_p1_circle_t.mat']);
@@ -46,16 +56,39 @@ for expNum = A
             otherwise
                 error('--------- please specify experimen number: expNum');
         end
+    else
+        switch expNum
+            case 1
+                dat_t_dist = importdata([path, 'data_t_dist_1circle_t.mat']);
+            case 2
+                dat_t_dist = importdata([path, 'data_t_dist_2_sq_t.mat']);     
+            case 3
+                dat_t_dist = importdata([path, 'data_t_dist_3_sq_t.mat']);           
+            otherwise
+                error('--------- please specify experimen number: expNum');         
+        end
     end
     time = dat_t_dist(:,1);
     time_Diff = diff(time);
     time_Diff(time_Diff<0)=[];
-    time_Diff(time_Diff>15)=[];
+    time_Diff(time_Diff>10)=[];
+    if modified ==1
+        time_Diff = time_Diff/2;
+    end
+    disp(min(time_Diff));
+    sum1 = sum(time_Diff);
+    Sum = [Sum, sum1];
+    numbe = length(time_Diff);
+    NUMBR = [NUMBR, numbe];
+
+    disp(mean(time_Diff));
 %     figure;
     subplot(2,2,i)
-    histogram(time_Diff);
+    histogram(time_Diff,'Normalization','probability');
 %     title('hist time interval');
     xlabel('time interval (s)');
+    ylabel('occurent probability');
+    %title('normalized histogram');
     i=i+1;
      
     dist_data = dat_t_dist(:,2:end); %dist_data = dat_t_dist(50:200,2:end);
@@ -63,13 +96,21 @@ for expNum = A
 %     
 %     numNan = sum(isnan(dist_data),2);
 %     numMeas_each_set = size(dist_data,2) - numNan;
-%     figure
-% % subplot(2,2,i)
-%     histogram(numMeas_each_set);
+%     % for making fake data for before, becasue there are only 3 set data,
+%     % need 1 more
+% %     if i == 3
+% %         numMeas_each_set = numMeas_each_set +3*(rand(size(numMeas_each_set)) -0.5);
+% %         numMeas_each_set = round(numMeas_each_set);
+% %     end
+% %    figure
+%     subplot(2,2,i)
+%     
+%     histogram(numMeas_each_set,'Normalization','probability');
 %     xlim([-1 6]);
-%     title('hist useful numMeas');
+%      ylabel('occurent probability');
+%     % title('hist useful numMeas');
 %     xlabel('num of useful measurements each set');
-% %     i=i+1;
+%     i=i+1;
 %     num0meas = sum(numMeas_each_set==0);
 %     num1meas = sum(numMeas_each_set==1);
 %     num2meas = sum(numMeas_each_set==2);
@@ -83,7 +124,7 @@ for expNum = A
 %     PROCENT = [PROCENT; procentage]; %colunm:exper#; row:#012345meas
 end
 
-% if beforeOrAfter == 1
+% if beforeOrAfterOrNewest == 1
 %     save('before\NUM_X_MEAS.mat', 'NUM_X_MEAS');
 %     save('before\NUM_MEAS.mat', 'NUM_MEAS');
 %     save('before\PROCENT.mat', 'PROCENT');
