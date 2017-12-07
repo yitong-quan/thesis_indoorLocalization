e9160ff1330a90b5322cac70e912043cdcd9ddd7
yitong-quan <xmlysxr@gmail.com>
2017-12-07 17:47:24 +0100

    + RT2UWB traj

--- data-from-experiments/experiment_24.Nov.2017.Hangar/alighment/forAlignment_basedOnNodes.m
+++ data-from-experiments/experiment_24.Nov.2017.Hangar/alighment/forAlignment_basedOnNodes.m
@@ -15,15 +15,31 @@ daspect([10,10,10]);
 % M0 = [ [cos(theta), -sin(theta); sin(theta), cos(theta)], [t1,t2]'];
 x0 = 100*rand(1,3); % x = [theta, t1, t2]
 options = optimoptions(@lsqnonlin,'Algorithm','levenberg-marquardt','Display','iter','MaxIterations',2000);
-[x,resnorm] = lsqnonlin(@myfun,x0,[],[],options);
+resnorm_last = inf;
+RESNORM = [];
+x_last = x0;
+for ii = 1:10
+    [x,resnorm] = lsqnonlin(@myfun,x0,[],[],options);
+    
+    if resnorm < resnorm_last
+        x_opt = x;
+        resnorm_opt = resnorm;
+        resnorm_last = resnorm;
+        RESNORM =  [RESNORM, resnorm];    
+    end
+    x0 = x + 10*rand(size(x_opt));
+end    
 
-M = [ [cos(x(1)), -sin(x(1)); sin(x(1)), cos(x(1))], x(2:3)'];
+M = [ [cos(x_opt(1)), -sin(x_opt(1)); sin(x_opt(1)), cos(x_opt(1))], x_opt(2:3)'];
 node_MoCap3_RT = M(:,[1,2]) * node_MoCap3 + M(:,end);
 plot(node_MoCap3_RT(1,:),node_MoCap3_RT(2,:),'xr');
-title_string = ['theta=', num2str(wrapTo2Pi(x(1))),', t1=', num2str(x(2)),', t2=', num2str(x(3)),num2str(x(2)),', resnorm=', num2str(resnorm)];
+title_string = ['theta=', num2str(wrapTo2Pi(x_opt(1))),', t1=', num2str(x_opt(2)),', t2=', num2str(x_opt(3)), ', resnorm=', num2str(resnorm_opt)];
 title(title_string);
 legend('node UWB(orig)', 'node MoCap 3(orig)', 'node MoCap 3(RT)');
 % afterRT = R0 * nodes_optimal + t0;
+
+figure; plot(RESNORM, '-*');
+RESNORM
 
 function F = myfun(xx)
 node_UWB = importdata('..\UWB\output_algo\nodesPositionLaserOptimal\nodePo.mat');
---
+++ data-from-experiments/experiment_24.Nov.2017.Hangar/mocap/afterRTtoUWB/RT2UWB.m
@@ -0,0 +1,6 @@
+x = importdata('..\..\alighment\RT_map_MoCap_to_UWB_theataT1T2.mat');
+M = [ [cos(x(1)), -sin(x(1)); sin(x(1)), cos(x(1))], x(2:3)'];
+PoMC = cortexjson7sq(:,[4,5]);
+PoMC_RT =  M(:,[1,2]) * PoMC' + M(:,end);
+figure;plot(PoMC_RT(1,:),PoMC_RT(2,:))
+cortexjson7sq = [cortexjson7sq, PoMC_RT'];
