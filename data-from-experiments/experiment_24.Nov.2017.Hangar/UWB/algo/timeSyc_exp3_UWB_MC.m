39e033aa097d61386267b7903eb9b25a98a864ec
yitong-quan <xmlysxr@gmail.com>
2017-12-18 22:41:55 +0100

    update

--- data-from-experiments/experiment_24.Nov.2017.Hangar/UWB/algo/timeSyc_exp3_UWB_MC.m
+++ data-from-experiments/experiment_24.Nov.2017.Hangar/UWB/algo/timeSyc_exp3_UWB_MC.m
@@ -5,3 +5,5 @@ UWB_ts = data(UWB_idx,1);
 MC_ts = MoCap_data_shrinked(MC_idx,9);
 time_gaps =UWB_ts- MC_ts;
 time_gaps_mean = mean(time_gaps); % time_gaps_mean = -8.36253712625157
+idx_alighment_UWB_MC = [UWB_idx; MC_idx];
+save('..\..\alighment\idx_alighment_UWB_MC_exp3.mat', 'idx_alighment_UWB_MC');
